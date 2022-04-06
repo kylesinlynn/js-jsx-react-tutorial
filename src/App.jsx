77a42b404ab5cc93060b24596cbfc7bb535b8d10
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-04-06 10:15:18 +0630

    done todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,60 @@
 import './App.css'
 import { useState, useRef } from 'react'
 
-const Fruits = ['Apple', 'Orange']
+const TodoList = [
+    {
+        id: 1,
+        title: 'Learn React',
+        completed: false
+    }
+]
 
 const App = () => {
+    const [todo, setTodo] = useState(TodoList)
+    const newTodo = useRef()
 
-    const [fruits, setFruits] = useState(Fruits)
-    const newFruit = useRef('')
-
-    const submitFunction = (event) => {
-        event.preventDefault()
-        const fruit = newFruit.current.value
-        setFruits(prev => [...prev, fruit])
-        return (newFruit.current.value = '')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newTodoTitle = newTodo.current.value
+        if (newTodoTitle.trim()) {
+            setTodo([...todo, {
+                id: todo.length + 1,
+                title: newTodoTitle,
+                completed: false
+            }])
+            newTodo.current.value = ''
+        }
     }
-    
+
+    const handleToggle = (id) => {
+        setTodo(todo.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        }))
+    }
+
+    const handleDelete = (id) => {
+        setTodo(todo.filter(todo => todo.id !== id))
+    }
+
     return (
     <div className='App'>
-        <h1>Form</h1>
-        <form onSubmit={submitFunction}>
-            <input type="text" ref={newFruit} />
-            <input type="submit" />
+        <h1>ToDo Lists</h1>
+        <form onSubmit={handleSubmit}>
+            <input ref={newTodo} type='text' placeholder='Add Todo' />
+            <button type='submit'>Add</button>
         </form>
-        <p>{fruits.join(', ')}</p>
+        <ul>
+            {todo.map(todo => (
+                <li key={todo.id}>
+                    {todo.title}
+                    <input type='checkbox' checked={todo.completed} onChange={() => handleToggle(todo.id)} />
+                    <button onClick={() => handleDelete(todo.id)}>X</button>
+                </li>
+            ))}
+        </ul>
     </div>
     )
 }
